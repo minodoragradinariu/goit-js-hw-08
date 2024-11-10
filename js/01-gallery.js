c6acd5b38155b31ef920a96b771717b9b7140f8c
minodoragradinariu <minodoragradinariu@yahoo.ro>
2024-11-10 18:47:37 +0200

    ex1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,33 @@
-// Add imports above this line
+// Add imports above this line 
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// @ts-ignore
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+          <a href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+          </a>
+        </li>
+      `;
+    })
+    .join('');
+}
+
+galleryContainer.innerHTML = createGalleryMarkup(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
