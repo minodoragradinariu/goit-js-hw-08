b73723481f10b1c5f7922b5ca10462706b729b76
minodoragradinariu <minodoragradinariu@yahoo.ro>
2024-11-10 18:48:27 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,6 @@
 // Add imports above this line 
 import { galleryItems } from './gallery-items';
 // Change code below this line
-// @ts-ignore
 import SimpleLightbox from "simplelightbox";
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
